ead6109e4adf3b84d08ea9cf3a5c64edd3356dbe
Kira7dn <89310517+Kira7dn@users.noreply.github.com>
2024-08-08 21:27:20 +0700

    payroll slip

--- components/dialog/PayrollDialog.tsx
+++ components/dialog/PayrollDialog.tsx
@@ -9,6 +9,7 @@ import {
 import { ScrollArea } from "@/components/ui//scroll-area";
 import { useState } from "react";
 import "./payroll-slip.css";
+import Image from "next/image";
 
 export const PayrollDialog = ({ children }: { children: React.ReactNode }) => {
   const [open, setOpen] = useState(false);
@@ -17,9 +18,32 @@ export const PayrollDialog = ({ children }: { children: React.ReactNode }) => {
     <Dialog open={open} onOpenChange={setOpen}>
       <DialogTrigger asChild>{children}</DialogTrigger>
       <DialogContent className="max-w-screen-md">
+        <DialogHeader></DialogHeader>
         <ScrollArea className="h-[80vh] rounded-md">
-          <div className="bg-white px-8 py-4">
-            <div className="mb-2 text-heading4-bold">Payslip</div>
+          <div className="bg-white px-8 py-4 ">
+            <div className="flex justify-between">
+              <div className="mb-2 flex items-center justify-start gap-2">
+                <Image
+                  src="/assets/logo.svg"
+                  height={32}
+                  width={32}
+                  alt="Logo"
+                  className="dark:hidden"
+                  priority
+                />
+
+                <div className="text-heading4-bold">Payslip</div>
+              </div>
+              <div>
+                <Image
+                  src="/assets/confidential.png"
+                  alt="confidential"
+                  width={160}
+                  height={40}
+                  className="rotate-12"
+                />
+              </div>
+            </div>
             <div className="flex justify-start border-y border-[#ccc] py-2">
               <div className="">
                 <div className="text-body-normal">PAY PERIOD</div>
@@ -31,7 +55,7 @@ export const PayrollDialog = ({ children }: { children: React.ReactNode }) => {
                 <div className="py-2">
                   <div className="text-body-bold">Piven El'Sync</div>
                   <div className="text-small-medium">
-                    mary.ann+Regr06@salarium.com
+                    maryannRegr06@salarium.com
                   </div>
                 </div>
                 <div className="">
@@ -74,182 +98,85 @@ export const PayrollDialog = ({ children }: { children: React.ReactNode }) => {
                     </div>
                   </div>
                 </div>
-                <div className="gross">
-                  <div className="title">Gross Income</div>
-                  <div className="entry">
-                    <div className="label"></div>
-                    <div className="value">92,823.86</div>
+              </div>
+              <div className="flex w-full flex-col gap-2 px-3 pb-2">
+                <div className="flex flex-col">
+                  <div className="flex w-full flex-col">
+                    <div className="text-base-medium">Salary</div>
+                    <div className="flex w-full justify-between">
+                      <div className="pl-2">Basic Salary</div>
+                      <div className="">1201511</div>
+                    </div>
+                    <div className="flex w-full justify-between">
+                      <div className="pl-2">Overtime</div>
+                      <div className="">1201511</div>
+                    </div>
+                    <div className="flex w-full justify-between">
+                      <div className="pl-2">Holiday</div>
+                      <div className="">1201511</div>
+                    </div>
+                  </div>
+                  <div className="flex w-full flex-col">
+                    <div className="text-base-medium">Leave</div>
+                    <div className="flex w-full justify-between">
+                      <div className="pl-2">Paid leave</div>
+                      <div className="">1201511</div>
+                    </div>
+                    <div className="flex w-full justify-between">
+                      <div className="pl-2">Unpaid leave</div>
+                      <div className="">(1201511)</div>
+                    </div>
+                  </div>
+                  <div className="flex w-full flex-col">
+                    <div className="text-base-medium">Allowance</div>
+                    <div className="flex w-full justify-between">
+                      <div className="pl-2">Position</div>
+                      <div className="">1201511</div>
+                    </div>
+                    <div className="flex w-full justify-between">
+                      <div className="pl-2">Travel</div>
+                      <div className="">(1201511)</div>
+                    </div>
+                  </div>
+                  <div className="flex w-full justify-between bg-black  bg-opacity-5 text-base-semibold">
+                    <div className="label">TOTAL INCOME</div>
+                    <div className="amount">82,705.06</div>
                   </div>
                 </div>
-              </div>
-              <div className="">
-                <div className="details">
-                  <div className="basic-pay">
-                    <div className="entry">
-                      <div className="label">Basic Pay</div>
-                      <div className="amount">45,000.00</div>
+                <div className="flex flex-col">
+                  <div className="flex w-full flex-col">
+                    <div className="text-base-medium">Tax & Insurance</div>
+                    <div className="flex w-full justify-between">
+                      <div className="pl-2">Social Insurance (8%)</div>
+                      <div className="">(1201511)</div>
+                    </div>
+                    <div className="flex w-full justify-between">
+                      <div className="pl-2">Health Insurance (1.5%)</div>
+                      <div className="">(1201511)</div>
+                    </div>
+                    <div className="flex w-full justify-between">
+                      <div className="pl-2">Unemployment insurance (1%)</div>
+                      <div className="">(1201511)</div>
+                    </div>
+                    <div className="flex w-full justify-between">
+                      <div className="pl-2">Income Tax</div>
+                      <div className="">(1201511)</div>
                     </div>
                   </div>
-                  <div className="salary">
-                    <div className="entry">
-                      <div className="label">Salary</div>
-                      <div className="detail"></div>
-                      <div className="amount"></div>
+                  {/* <div className="flex w-full flex-col">
+                    <div className="text-base-medium">Other Reduction</div>
+                    <div className="flex w-full justify-between">
+                      <div className="pl-2">Overtime</div>
+                      <div className="">1201511</div>
                     </div>
-                    <div className="entry">
-                      <div className="label"></div>
-                      <div className="detail">Undertime</div>
-                      <div className="amount">(33,231.36)</div>
+                    <div className="flex w-full justify-between">
+                      <div className="pl-2">Holiday</div>
+                      <div className="">1201511</div>
                     </div>
-                    <div className="entry">
-                      <div className="label"></div>
-                      <div className="detail">Unworked Holiday</div>
-                      <div className="amount">4,153.92</div>
-                    </div>
-                    <div className="entry">
-                      <div className="label"></div>
-                      <div className="detail">
-                        Regular Holiday Regular Holiday
-                      </div>
-                      <div className="amount">7,009.65</div>
-                    </div>
-                    <div className="entry">
-                      <div className="label"></div>
-                      <div className="detail">
-                        Regular Holiday Regular Holiday Night
-                      </div>
-                      <div className="amount">5,997.11</div>
-                    </div>
-                    <div className="entry">
-                      <div className="label"></div>
-                      <div className="detail">Night</div>
-                      <div className="amount">15,992.59</div>
-                    </div>
-                    <div className="entry">
-                      <div className="label"></div>
-                      <div className="detail">Regular Holiday</div>
-                      <div className="amount">4,673.07</div>
-                    </div>
-                    <div className="entry">
-                      <div className="label"></div>
-                      <div className="detail">Regular Holiday Night</div>
-                      <div className="amount">3,998.05</div>
-                    </div>
-                    <div className="entry">
-                      <div className="label"></div>
-                      <div className="detail">
-                        Regular Holiday Night Overtime
-                      </div>
-                      <div className="amount">1,485.00</div>
-                    </div>
-                    <div className="entry">
-                      <div className="label"></div>
-                      <div className="detail">Special Holiday</div>
-                      <div className="amount">3,037.50</div>
-                    </div>
-                    <div className="entry">
-                      <div className="label"></div>
-                      <div className="detail">Special Holiday Night</div>
-                      <div className="amount">2,598.75</div>
-                    </div>
-                    <div className="entry">
-                      <div className="label"></div>
-                      <div className="detail">Rest Day</div>
-                      <div className="amount">2,700.00</div>
-                    </div>
-                  </div>
-                  <div className="leaves">
-                    <div className="entry">
-                      <div className="label">Leaves</div>
-                      <div className="detail"></div>
-                      <div className="amount"></div>
-                    </div>
-                    <div className="entry paid">
-                      <div className="label"></div>
-                      <div className="detail">Paid Leave</div>
-                      <div className="amount">2,076.92</div>
-                    </div>
-                    <div className="entry unpaid">
-                      <div className="label"></div>
-                      <div className="detail">Unpaid Leave</div>
-                      <div className="amount">(2076.96)</div>
-                    </div>
-                  </div>
-                  <div className="taxable_allowance">
-                    <div className="entry">
-                      <div className="label">Taxable Allowance</div>
-                      <div className="detail"></div>
-                      <div className="amount"></div>
-                    </div>
-                    <div className="entry">
-                      <div className="label"></div>
-                      <div className="detail">Allowance Name</div>
-                      <div className="amount">1,000.00</div>
-                    </div>
-                  </div>
-                  <div className="taxable_bonus">
-                    <div className="entry">
-                      <div className="label">Taxable Bonus</div>
-                      <div className="detail"></div>
-                      <div className="amount"></div>
-                    </div>
-                    <div className="entry">
-                      <div className="label"></div>
-                      <div className="detail">Bonus Name</div>
-                      <div className="amount">19,409.34</div>
-                    </div>
-                  </div>
-                  <div className="taxable_commission"></div>
-
-                  <div className="nti">
-                    <div className="entry">
-                      <div className="label">TAXABLE INCOME</div>
-                      <div className="detail"></div>
-                      <div className="amount">82,705.06</div>
-                    </div>
-                  </div>
-                  <div className="withholding_tax">
-                    <div className="entry">
-                      <div className="label">Withholding Tax</div>
-                      <div className="detail"></div>
-                      <div className="amount">(21,548.85)</div>
-                    </div>
-                  </div>
-                  <div className="non_taxable_allowance">
-                    <div className="entry">
-                      <div className="label">Non-Taxable Allowance</div>
-                      <div className="detail"></div>
-                      <div className="amount"></div>
-                    </div>
-                    <div className="entry">
-                      <div className="label"></div>
-                      <div className="detail">Allowance Name</div>
-                      <div className="rate"></div>
-                      <div className="amount">1,500.00</div>
-                    </div>
-                  </div>
-                  <div className="non_taxable_bonus">
-                    <div className="entry">
-                      <div className="label">Non-Taxable Bonus</div>
-                      <div className="detail"></div>
-                      <div className="rate"></div>
-                      <div className="amount"></div>
-                    </div>
-                    <div className="entry">
-                      <div className="label"></div>
-                      <div className="detail">Bonus Name</div>
-                      <div className="rate"></div>
-                      <div className="amount">2,000.00</div>
-                    </div>
-                  </div>
-
-                  <div className="net_pay">
-                    <div className="entry">
-                      <div className="label">NET PAY</div>
-                      <div className="detail"></div>
-                      <div className="rate"></div>
-                      <div className="amount">69,656.21</div>
-                    </div>
+                  </div> */}
+                  <div className="flex w-full justify-between bg-black  bg-opacity-5 text-base-semibold">
+                    <div className="label">NET PAY</div>
+                    <div className="amount">82,705.06</div>
                   </div>
                 </div>
               </div>
